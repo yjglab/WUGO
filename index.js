c62e2b307b590255d36186a17bb3f6a4250ca8e8
yjglab <yjgdesign@gmail.com>
2020-11-09 03:23:45 +0900

    2nd, made server and used babel in node

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hi!");
+import express from "express";
+// const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`listening on: http://localhost:${PORT}`);
+// function handleListening() {
+//   console.log(`listening on: http://localhost:${PORT}`);
+// }
+
+const handleHome = (req, res) => res.send("Hello from home");
+// function handleHome(req, res) {
+//   res.send("Hello from home");
+// }
+
+const handleProfile = (req, res) => res.send("You r on my profile");
+// function handleProfile(req, res) {
+//   res.send("You r on my profile");
+// }
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+app.listen(4000, handleListening);
