f8c4c3752db901ecd5392dc222467d042753192e
yjglab <yjgdesign@gmail.com>
2020-11-10 03:08:09 +0900

    #3. add middleware

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import express from "express";
 // const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () =>
   console.log(`listening on: http://localhost:${PORT}`);
-// function handleListening() {
-//   console.log(`listening on: http://localhost:${PORT}`);
-// }
 
 const handleHome = (req, res) => res.send("Hello from home");
-// function handleHome(req, res) {
-//   res.send("Hello from home");
-// }
 
 const handleProfile = (req, res) => res.send("You r on my profile");
-// function handleProfile(req, res) {
-//   res.send("You r on my profile");
-// }
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
+
 app.listen(4000, handleListening);
